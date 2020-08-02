31a8827dbe6a81fb07e67e49ca56e66c1dcc5aba
elchung <ericlewischung@gmail.com>
2020-08-02 13:55:56 -0500

    test work making ability score table look okay

--- pages/CharacterSheet/AbilityScoresComponent.jsx
+++ pages/CharacterSheet/AbilityScoresComponent.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
 import Switch from '@material-ui/core/Switch';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Paper from '@material-ui/core/Paper';
 import BaseAbilityScoreComponent from './BaseAbilityScoreComponent'
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 
 export const AbilityScoreComponent = ({
     scoreOnTop, setScoreOnTop,
@@ -15,54 +16,69 @@ export const AbilityScoreComponent = ({
     chaScore, setChaScore
 }) => {
   return (
-    <Paper Elevation={3}>
-      <FormControlLabel
-        control={
-          <Switch
-            size="small"
-            checked={!scoreOnTop}
-            onChange={() => setScoreOnTop(state => !state)}
-            color="primary"
+    <Paper Elevation={3} width={10}>
+      <Grid container spacing={4} direction="column" justify="center" alignItems="center" wrap="nowrap">
+        <Grid item>
+          <FormControlLabel
+            control={
+              <Switch
+                size="small"
+                checked={!scoreOnTop}
+                onChange={() => setScoreOnTop(state => !state)}
+                color="primary"
+              />
+            }
           />
-        }
-        label="Small"
-      />
-      <BaseAbilityScoreComponent
-        ability="Strength"
-        abilityScore={strScore}
-        setAbilityScore={setStrScore}
-        scoreOnTop={scoreOnTop}
-      />
-      <BaseAbilityScoreComponent
-        ability="Dexterity"
-        abilityScore={dexScore}
-        setAbilityScore={setDexScore}
-        scoreOnTop={scoreOnTop}
-      />
-      <BaseAbilityScoreComponent
-        ability="Constitution"
-        abilityScore={conScore}
-        setAbilityScore={setConScore}
-        scoreOnTop={scoreOnTop}
-      />
-      <BaseAbilityScoreComponent
-        ability="Intellegence"
-        abilityScore={intScore}
-        setAbilityScore={setIntScore}
-        scoreOnTop={scoreOnTop}
-      />
-      <BaseAbilityScoreComponent
-        ability="Wisdon"
-        abilityScore={wisScore}
-        setAbilityScore={setWisScore}
-        scoreOnTop={scoreOnTop}
-      />
-      <BaseAbilityScoreComponent
-        ability="Charisma"
-        abilityScore={chaScore}
-        setAbilityScore={setChaScore}
-        scoreOnTop={scoreOnTop}
-      />
+        </Grid>
+        <Grid item>
+          <BaseAbilityScoreComponent
+            ability="Strength"
+            abilityScore={strScore}
+            setAbilityScore={setStrScore}
+            scoreOnTop={scoreOnTop}
+          />
+        </Grid>
+        <Grid item>
+          <BaseAbilityScoreComponent
+            ability="Dexterity"
+            abilityScore={dexScore}
+            setAbilityScore={setDexScore}
+            scoreOnTop={scoreOnTop}
+          />
+        </Grid>
+        <Grid item>
+          <BaseAbilityScoreComponent
+            ability="Constitution"
+            abilityScore={conScore}
+            setAbilityScore={setConScore}
+            scoreOnTop={scoreOnTop}
+          />
+        </Grid>
+        <Grid item>
+          <BaseAbilityScoreComponent
+            ability="Intellegence"
+            abilityScore={intScore}
+            setAbilityScore={setIntScore}
+            scoreOnTop={scoreOnTop}
+          />
+        </Grid>
+        <Grid item>
+          <BaseAbilityScoreComponent
+            ability="Wisdon"
+            abilityScore={wisScore}
+            setAbilityScore={setWisScore}
+            scoreOnTop={scoreOnTop}
+          />
+        </Grid>
+        <Grid item>
+          <BaseAbilityScoreComponent
+            ability="Charisma"
+            abilityScore={chaScore}
+            setAbilityScore={setChaScore}
+            scoreOnTop={scoreOnTop}
+          />
+        </Grid>
+      </Grid>
     </Paper>
   );
 }
--- pages/CharacterSheet/BaseAbilityScoreComponent.jsx
+++ pages/CharacterSheet/BaseAbilityScoreComponent.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
-import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import FormControl from '@material-ui/core/FormControl';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import scoreToModifier from '../Utils/abilityScoreUtils';
+import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    width: 300,
+    margin: 100,
+  },
+  resize: {
+    fontSize: 50
+  },
+};
 
 export const BaseAbilityScoreComponent = ({ ability, abilityScore, setAbilityScore, scoreOnTop }) => {
-  const [displayedAbilityScore, setDisplayedAbilityScore] = React.useState();
+  const [displayedAbilityScore, setDisplayedAbilityScore] = React.useState(abilityScore);
   const mainText = scoreOnTop ? displayedAbilityScore : scoreToModifier(abilityScore);
   const subText = scoreOnTop ? scoreToModifier(abilityScore) : displayedAbilityScore;
-
-  console.log(ability)
 
   const handleChange = (event) => {
     const numberInput = event.target.value.replace(/[^0-9]/g, '');
@@ -23,30 +38,40 @@ export const BaseAbilityScoreComponent = ({ ability, abilityScore, setAbilitySco
   };
 
   return (
-    <Paper Elevation={5}>
-      <Typography alight="center" nowrap>
-        {`${ability}`}
-      </Typography>
-      <FormControl variant="outlined">
-        <OutlinedInput
-          id={`${ability}-ability-maintext`}
-          value={mainText}
-          onChange={handleChange}
-          disabled={!scoreOnTop}
-          color={scoreOnTop ? 'Primary' : 'Secondary'}
-        />
-      </FormControl>
-      <FormControl variant="outlined">
-        <OutlinedInput
-          id={`${ability}-ability-subtext`}
-          value={subText}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          disabled={scoreOnTop}
-          color={scoreOnTop ? 'Secondary' : 'Primary'}
-        />
-      </FormControl>
-    </Paper>
+    <Card elevation={5} variant="outlined">
+      <CardContent>
+        <Typography alight="center" nowrap color="textSecondary" gutterBottom variant="caption">
+          {`${ability}`}
+        </Typography>
+        <div>
+          <TextField
+            id={`${ability}-ability-maintext`}
+            value={mainText}
+            onChange={handleChange}
+            disabled={!scoreOnTop}
+            color={scoreOnTop ? 'Primary' : 'Secondary'}
+            variant='outlined'
+            inputProps={{
+              style: {fontSize: 30, width: 30, height: 20}
+            }}
+          />
+        </div>
+        <div>  
+          <TextField
+            id={`${ability}-ability-subtext`}
+            value={subText}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            disabled={scoreOnTop}
+            color={scoreOnTop ? 'Secondary' : 'Primary'}
+            variant='outlined'
+            inputProps={{
+              style: {fontSize: 15, width: 15, height: 5}
+            }}
+          />
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
