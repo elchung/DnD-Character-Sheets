07e26b915ff6c413bc53d388c36b6232c330c510
elchung <ericlewischung@gmail.com>
2020-08-23 10:25:01 -0500

    shiftregtest update

--- misc/shiftRegTest.ino
+++ misc/shiftRegTest.ino
@@ -1,11 +1,3 @@
-//programming--
-//pb3 (pin 17) mosi
-//pb4 (pin 18) miso
-//pb5 (pin 19) sck
-
-#include <Adafruit_NeoPixel.h>
-
-
 //switch pins
 const int swMin = 5;
 const int swHr = 6;
@@ -17,17 +9,15 @@ const int dataPin = A2;
 const int latchPin = A3; // srclk, storage register clock pin
 const int clockPin = 1;
 const int serialClear = 0;  //keep high
-int s = 0;
-int m = 0;
-int h = 0;
+int time_second = 0;
+int time_minute = 0;
+int time_hour = 0;
 
-
-int abcdabcd[] = {
+byte abcd[] = {
   0,   8,   4,   12,  2,   10,  6,   14,  1,   9,  128, 136, 132, 140, 130, 138, 134, 142, 129, 137,
   64,  72,  68,  76,  66,  74,  70,  78,  65,  73, 192, 200, 196, 204, 194, 202, 198, 206, 193, 201,
   32,  40,  36,  44,  34,  42,  38,  46,  33,  41, 160, 168, 164, 172, 162, 170, 166, 174, 161, 169
 };
-
 
 void checkButtonPress(void (*)());
 void incrementRTCMinute();
@@ -43,7 +33,8 @@ void setup(){
   pinMode(serialClear, OUTPUT); //keep high
   digitalWrite(serialClear, HIGH);
 
-  Serial.begin(9600);
+  delay(20);
+  updateTime();
 }
 
 void loop() {
@@ -51,14 +42,14 @@ void loop() {
   checkButtonPress(swHr,  lastSwHrState,  incrementRTCHour);
 }
 
-void checkButtonPress(int sw, boolean lastState, void (*incrementFunc)()) {
+void checkButtonPress(int sw, int &lastState, void (*incrementFunc)()) {
   boolean buttonState = digitalRead(sw);
   if ((buttonState == HIGH) && (lastState == LOW)) {
     delay(20);
     boolean newState = digitalRead(sw);
     if (newState == HIGH) {
-      (*incrementFunc)();
       lastState = HIGH;
+      (*incrementFunc)();
     }
   } else if ((buttonState == LOW) && (lastState == HIGH)) {
     delay(20);
@@ -70,49 +61,25 @@ void checkButtonPress(int sw, boolean lastState, void (*incrementFunc)()) {
 }
 
 void incrementRTCMinute() {
-  m = m  + 1;
-  if (m > 9) {
-    m = 0;
-  }
-  digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin, h);
-  shiftOut(dataPin, clockPin, m);
-  shiftOut(dataPin, clockPin, s);
-  digitalWrite(latchPin, HIGH);
+  time_minute++;
+  if (time_minute > 59) { time_minute = 0; }
+  updateTime();
 }
 
 void incrementRTCHour() {
-  h = h + 1;
-  if (h > 9) {
-    h = 0;
+  time_hour++;
+  if (time_hour > 59) {
+    time_hour = 0;
   }
+  updateTime();
+}
+
+void updateTime() {    
   digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin, h);
-  shiftOut(dataPin, clockPin, m);
-  shiftOut(dataPin, clockPin, s);
+
+  shiftOut(dataPin, clockPin, MSBFIRST, abcd[time_second]); 
+  shiftOut(dataPin, clockPin, MSBFIRST, abcd[time_minute]); 
+  shiftOut(dataPin, clockPin, MSBFIRST, abcd[time_hour]); 
+
   digitalWrite(latchPin, HIGH);
 }
-
-void shiftOut(int myDataPin, int myClockPin, byte myDataOut) {
-  int i=0;
-  int pinState;
-  pinMode(myClockPin, OUTPUT);
-  pinMode(myDataPin, OUTPUT);
-  digitalWrite(myDataPin, 0);
-  digitalWrite(myClockPin, 0);
-
-  for (i=7; i>=0; i--)  {
-    digitalWrite(myClockPin, 0);
-    if ( myDataOut & (1<<i) ) {
-      pinState= 1;
-    }
-    else {  
-      pinState= 0;
-    }
-    digitalWrite(myDataPin, pinState);
-    digitalWrite(myClockPin, 1);
-    digitalWrite(myDataPin, 0);
-  }
-  digitalWrite(myClockPin, 0);
-}
-
